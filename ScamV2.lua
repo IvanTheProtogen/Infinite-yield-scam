a6f74042f24a7a3c180ceea03ea3c40aa2da2244
IvanTheProtogen <122665922+IvanTheProtogen@users.noreply.github.com>
2024-02-13 19:36:01 +0300

    Create ScamV2.lua

---
+++ ScamV2.lua
@@ -0,0 +1,15 @@
+-- I see you lil nigger
+
+local a = game:GetService('ReplicatedStorage').DefaultChatSystemChatEvents.SayMessageRequest
+
+repeat
+	a:FireServer("SUСК YОUR СОСКS, I GOT A BIGGER ONE.",'All')
+	a:FireServer("GULР GULР СRYВАВY, SАD СUZ RАРЕRD.","All")
+	a:FireServer("I гаре сhildгеп wооhоо!","All")
+	a:FireServer("Аhh..~~ Thаt's а nice colck, dаddу..~","All")
+	a:FireServer("That's a real goodanus..~","All")
+	a:FireServer("DADDY~ I'M GOING TO СUМ~ 💕💕","All")
+	a:FireServer("Who's a good mommy~ Who's a good mommy~ That's right, it's you cow girl~","All")
+	a:FireServer("HEY SLΑVЕ, LETS HAVE OIL SЕΧ","All")
+	task.wait()
+until false
